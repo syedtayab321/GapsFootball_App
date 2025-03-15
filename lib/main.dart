46c29365a54a3147f589f542268b69744345c452
Faisal Aslam <faisalaslam218@gmail.com>
2025-03-15 06:50:30 +0500

    Bug Fixing

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:gaps_football_app/CommonScreens/welcome.dart';
-import 'package:gaps_football_app/UserPortal/UserDashboardPage.dart';
 import 'package:get/get.dart';
 
-import 'CommonScreens/Login.dart';
 void main() {
   runApp(
-      DevicePreview(
-          enabled: !kReleaseMode,
-          builder: (context) => const MyApp(),
-        )
+    const MyApp(),
   );
 }
 
